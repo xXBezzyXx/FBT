d8039721c596668c69141b6df3b7d165f42b9511
xXBezzyXx <popmods@gmail.com>
2026-06-29 09:46:54 -0400

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// This is your Google Apps Script Web App URL.
-// This is the link that ends in /exec.
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwWoXyrT3zUwWNhz5u9k8gXHnqJeZS7BFXy1F6hSDnGTyv8_W-JKCBodciStaIFbQdE8w/exec';
